0e01a06b965706713f86f0266de14f8d895730e9
vedantparanjape <22630228+VedantParanjape@users.noreply.github.com>
2020-04-12 20:17:50 +0530

    changed generate_log_message_timestamp to add Unix Line ending character (\n) to every log message

--- utils.cpp
+++ utils.cpp
@@ -9,7 +9,7 @@
  */
 char* generate_log_message_timestamp(uint32_t timestamp, char* log_message)
 {
-    std::string log_string = "(" + std::string(std::to_string(timestamp)) + ") " + std::string(log_message);
+    std::string log_string = "(" + std::string(std::to_string(timestamp)) + ") " + std::string(log_message) + "\n";
 
     char *c_log_string = (char*) malloc(sizeof(char)*(log_string.size()+1));
     memcpy(c_log_string, log_string.c_str(), log_string.size()+1);
